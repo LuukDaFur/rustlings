0ec7dc76698a99ed1f192ccdaf90dd779383ef0e
Luuk Stassen <lhc.stassen@gmail.com>
2023-11-18 01:01:20 +0100

    Functions complete

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);
@@ -12,4 +10,5 @@ fn main() {
 
 fn square(num: i32) -> i32 {
     num * num;
+    return num;
 }
